84e4290cc99d2be8f50cce11d303ed53f82d2eb6
caous <caous.g@gmail.com>
2023-07-12 17:31:14 -0300

    first code

---
+++ BasicRabbit/Publisher/Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RabbitMQ.Client;
+using System.Text;
+
+string queueName = "rabbit_poc";
+
+try
+{
+    var factory = new ConnectionFactory() { 
+        HostName = "localhost"
+    };
+
+
+
+    using var connection = factory.CreateConnection();
+
+    using var chanel = connection.CreateModel();
+
+    chanel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+    chanel.BasicPublish(exchange: "",
+                                         routingKey: queueName,
+                                         basicProperties: null,
+                                         body: Encoding.UTF8.GetBytes("primeira mensagem"));
+
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine("error: " + ex.Message);
+}
